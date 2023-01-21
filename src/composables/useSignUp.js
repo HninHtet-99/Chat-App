95e4e0417d8e0b5e8cbeb216999492d9db0c1da2
HninHtet-99 <hninhtet1113@gmail.com>
2023-01-21 22:44:23 +0630

    fix login error

--- src/composables/useSignUp.js
+++ src/composables/useSignUp.js
@@ -4,7 +4,7 @@ import {auth} from '../firebase/config';
 let error = ref(null);
 let createAccount =async(email,userName,password)=>{
     try {
-        let res = await auth.createUserWithEmailAndPassword(email,userName,password)
+        let res = await auth.createUserWithEmailAndPassword(email,password)
         if (!res) {
           throw new Error('Could not create new account! Try Again!');
         }
